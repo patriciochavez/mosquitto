68af6c9d0a731d99422d970c23fcd2b9f9ffed70
patriciochavez <patricio_chavez@hotmail.com>
2018-02-28 15:52:33 -0300

    port exposed

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker build -t patriciochavez/mosquitto .
-sudo docker run -d --name mosquitto patriciochavez/mosquitto
+sudo docker run -d --name mosquitto -p 1883:1883 patriciochavez/mosquitto
--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker ps --all | grep mosquitto | awk {'print $1}' | xargs sudo docker rm -f
 sudo docker build -t patriciochavez/mosquitto .
-sudo docker run -d --name mosquitto patriciochavez/mosquitto
+sudo docker run -d --name mosquitto -p 1883:1883 patriciochavez/mosquitto
